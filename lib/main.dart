3580574dffb35fa906f2ddc8909667c21d2ff845
ulisses <uli.citrinitas@gmail.com>
2026-03-17 09:11:31 -0300

    adicionado tema da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +11,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "Exercícios Flutter",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
+        useMaterial3: true,
+        brightness: Brightness.dark
+    ),
+    home: const HomeScreen(),
     );
   }
 }
